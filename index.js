b6e50b8bf951ee54c9cc3a3e70acd43f3f1680b4
Victor Elci <victor.elci@aboveproperty.com>
2023-03-03 11:38:46 -0500

    fix rebase

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-spreadsheet-export',
-
-  included() {
-    this._super.included.apply(this, arguments);
-    this.import(`${this.treePaths.vendor}/Blob.js`);
-  },
+  name: require('./package').name,
 };
